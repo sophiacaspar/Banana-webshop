e430fd773db092163cef3c07bc03a2963a7ef7ab
Sophia Caspár <sophiacaspar@gmail.com>
2016-01-08 14:03:37 +0100

    removed connect file

--- db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'utbweb.its.ltu.se';
-$db_name = 'angbru0db';
-$username = 'angbru-0';
-$password = 'sopcas-1';
-
-$link = mysql_connect($host, $username, $password);
-	mysql_select_db($db_name, $link);
-?>
